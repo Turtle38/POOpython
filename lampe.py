e09930d653f85c429792bb6ab39c8d384b3098f7
Daphné Di Malta <daphne38250@gmail.com>
2021-04-09 15:17:22 +0200

    lampe

---
+++ lampe.py
@@ -0,0 +1,21 @@
+class Lampe:
+    def __init__(self):
+        self.allumee=False
+    
+    def isOn(self):
+        if input("allumée ou eteind: ")=="allumée":
+            self.allumee=True
+        else:
+            self.allumee=False
+    
+    def lampePropriete(self):
+        if self.allumee==False:
+            print("éteind")
+        else:
+            print("allumée")
+    
+lampe=Lampe()
+lampe.isOn()
+lampe.lampePropriete()
+
+input()
