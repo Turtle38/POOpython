f1685c6172035cf3d2efaaf2e468bb033b6a2124
Théo Fontana <fontana.theo.me@gmail.com>
2021-04-09 13:50:31 +0200

    ajout d'un devoir LOL

--- class.py
+++ class.py
@@ -12,14 +12,19 @@ class Person:
         print("au revoir twa ;)")
     
     #################################
-######ecrire un programme sui demande au gens gens la taille et la couleur de la table####
+
+
+######ecrire un programme qui demande au gens gens la taille et la couleur de la table####
 class Table:
-    perimetre=""
-    couleur=""
+    perimetre = ""
+    couleur = ""
     #######fontions######
     def perimetre():
         perimetre=input("entres le perimetre de la table: ")
+
     def couleur():
         couleur=input("donnes la couleure de la table: ")
+    
+    #Declarer une fonction qui affiche les propriétés de la table :)
 
 input()
